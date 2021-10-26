38c4b46941594cdb2b37e910a48acaa4b989d1f9
Atik1000 <foysalkazi45@gmail.com>
2021-10-27 03:17:40 +0600

    bining attribute

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
-    <h1>{{greet}}  {{ message }}</h1>
-
+    <!-- mastached -->
+    <h1>{{ greet }} {{ message }}</h1>
+    <!-- v-text -->
     <div v-text="channel"></div>
-    <div v-text="message"></div>
+    <div v-html="message"></div>
+    <!-- binding attribute -->
+    <h2 v-bind:id="headingId">Heading</h2>
+    <button v-bind:disabled="isDisabled">Submit</button>
   </div>
 </template>
 
@@ -12,8 +16,10 @@ export default {
   data() {
     return {
       greet: "Hello",
-      message: "Welcome to Vue.js",
+      message: "<b>Welcome to Vue.js</b>",
       channel: "Vue.js",
+      headingId:'heading',
+      isDisabled:true
     };
   },
 };
