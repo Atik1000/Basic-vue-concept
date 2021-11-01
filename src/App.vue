8ba3c50383dee24645b93f8091530818ba4f1685
Atik1000 <foysalkazi45@gmail.com>
2021-11-02 02:12:41 +0600

    add

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,44 @@
 <template>
-  <div class="">
-    <h2 v-if="num===0">The number is zero</h2>
-    <h2 v-else-if="num===1">The number is one</h2>
-    <h2 v-else>The number is not zero or one</h2>
-<div v-if="display">
-    <button @click="num=0">Zero</button>
-    <button @click="num=1">One</button>
-    <button @click="num=2">Two</button>
-</div>
-<h1 v-show="showElement
-">Show V element</h1>
-</div>
+  <div id="app">
+    <div v-for="actor in actors" :key="actor.name">
+      <h2>{{actor.name}}</h2>
+      <h3 v-for="movie in actor.movies" :key="movie">{{movie}}</h3>
+      <h1 v-for="(value,key,index) in myInfo" :key="value">{{index}} {{key}} {{value}}</h1>
+    </div>
+
+    <div v-for="name in names" :key="name">
+      <h2>{{name}}</h2>
+      <hr>
+    </div>
+    </div>
+    
 </template>
 
 <script>
 export default {
   data() {
     return {
-     num: 10,
-     display:false,
-     showElement:true,
+        names: ['John', 'Jane', 'Jack'],
+      
+      actors:[
+        {name:'cristian bale',
+        movies:['batman','superman','ironman']
+        },
+        {name:'james bond',
+        movies:['spy','skyfall','casino royale']
+        },
+        {name:'tom hanks',
+        movies:['tom hanks','tom hanks','tom hanks']
+        }
+      ] ,
+      myInfo:{
+        name:'cristian bale',
+        // channels:['batman','superman','ironman'],
+        channels:'code',
+        course:"vue3"
+         
+      }
+
 };
 }
 }
