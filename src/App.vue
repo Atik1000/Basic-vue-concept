360a3ca5ec145324266c534a2757956f11e8d7ca
Atik1000 <foysalkazi45@gmail.com>
2021-11-03 15:23:56 +0600

    add

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
-  <div id="app">
-    <h1>{{name}}</h1>
-    <button v-on:click="name= 'Batman'">Change name</button>
-    <h1>{{count}}</h1>
-    <button v-on:click="count +=1">increment</button>
-    <!-- reset btn -->
-    <button v-on:click="count = 0">reset</button>
-    <button v-on:click="decrement(5)">decrement5</button>
-<button v-on:click="decrement(1)">decrement1</button>
+  <form>
+    <div>
+      <pre>
+    {{ JSON.stringify(formValues, null, 2) }}
 
-  </div>
+  </pre>
+      <br />
+      <hr />
+    </div>
+    <div>
+      <label for="name">Name</label>
+      <input id="name" v-model="formValues.name" />
+    </div>
+    <br />
+    <div>
+      <label for="profile">Profile summary</label>
+      <textarea id="profile" v-model="formValues.profile"></textarea>
+    </div>
+    <br />
+    <div>
+      <label for="country">Select country</label>
+      <select id="country" v-model="formValues.country">
+        <option value>Select country</option>
+        <option value="US">United States</option>
+        <option value="GB">United Kingdom</option>
+        <option value="CA">Canada</option>
+      </select>
+    </div>
+    <br />
+
+    <div>
+      <label for="job-location">Select job location</label>
+      <select id="job-location" multiple v-model="formValues.joblocation
+      ">
+        <option value>Select country</option>
+        <option value="US">United States</option>
+        <option value="GB">United Kingdom</option>
+        <option value="CA">Canada</option>
+      </select>
+    </div>
+    <br />
+    <div>
+      <label for="job-type">Select job type</label>
+      <input type="checkbox" v-model="formValues.jobtype" true-value="yes" false-value="no" />
+    </div>
+    <br />
+  </form>
 </template>
 
 <script>
@@ -17,15 +53,18 @@ export default {
   name: "App",
   data() {
     return {
-      name: "Vue.js",
-      count: 0
+      formValues: {
+        name: "",
+        profile: "",
+        country: "",
+        joblocation: [],
+        remotework: 'no',
+      }
     };
   },
   methods: {
-    decrement(num) {
-      this.count-=num;
-    },
-    
+
+
   },
 };
 </script>
